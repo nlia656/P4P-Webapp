89db9d2e9372d9dec544d95b0660b12cbcfc1b77
nlia656 <nlia656@aucklanduni.ac.nz>
2025-08-27 13:44:44 +1200

    start screen for name

--- Webapp/src/Home.jsx
+++ Webapp/src/Home.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <>
         <div>
             <label for="name">Please enter your full name: </label>
-            <input type="text" id="name"></input>
+            <input type="text" id="name" autocomplete='off'></input>
         </div>
-        <button></button>
+        <button style={{marginTop: 20}} onClick={()=>navigate("/player")}>Start</button>
     </>
   )
 }
